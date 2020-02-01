1674398ff729be7d179b39b30295a7fcd38f8c4d
michael otero <michaelotero1118@gmail.com>
2020-02-01 19:24:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,13 @@ end
 
 def assign_rooms(attendees)
   attendeeds.each_with_index.map do |attendee, index|
-    "Hello, #{attendee}!  "
+    "Hello, #{attendee}! You'll be assigned to room #{index =1}!"
+  end
+end
+
+def  printer(attendees)
+  
+    
     
  
   
