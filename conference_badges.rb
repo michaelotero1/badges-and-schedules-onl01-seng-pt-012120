a7bf37d4c557a209ff3ee2cd8d87654723aca858
michael otero <michaelotero1118@gmail.com>
2020-02-01 13:51:43 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+# Write your code here.\
+# need to use batch_badge_creator that will take an array of names 
+
+def batch_maker(name)
+  puts "Hello, my name is #{name}"
+end 
+
+  
+   
